d8b57cd38ac6d51c2669afcb39a9ba6f05e3f70c
Amit Mahindra <Amythcode@gmail.com>
2024-08-03 13:51:31 +0530

    application ready

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ scaler = pickle.load(open('scaling.pkl', 'rb'))
 def home():
     return render_template('home.html')
 
-@app.route('/predict', methods=['POST'])
+@app.route('/predict_api', methods=['POST'])
 def predict_api():
     data = request.json['data']
     print(np.array(list(data.values())).reshape(1,-1))
@@ -21,6 +21,15 @@ def predict_api():
     output = regmodel.predict(new_data)
     print(output[0])
     return jsonify(output[0])
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = scaler.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output = regmodel.predict(final_input)
+    return render_template('home.html', prediction_text = "The House Price Prediction is {}".format(output))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
